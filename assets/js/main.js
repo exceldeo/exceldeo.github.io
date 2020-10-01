6f8a7d4028e56444833b5dff6163e8a6e5f1c6a1
exceldeo <52096462+exceldeo@users.noreply.github.com>
2020-10-01 22:31:03 +0700

    cv

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+  const TypeWriter = function (txtElement, words, wait = 100) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+  }
+
+  TypeWriter.prototype.type = function () {
+      const current = this.wordIndex % this.words.length;
+      const fulltxt = this.words[current];
+
+      if (this.isDeleting) {
+          this.txt = fulltxt.substring(0, this.txt.length - 1);
+      } else {
+          this.txt = fulltxt.substring(0, this.txt.length + 1);
+      }
+
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+      let typeSpeed = 150;
+
+      if (this.isDeleting) {
+          typeSpeed /= 2;
+      }
+
+      if (!this.isDeleting && this.txt === fulltxt) {
+          typeSpeed = this.wait;
+          this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+          this.isDeleting = false;
+          this.wordIndex++;
+          typeSpeed = 500;
+      }
+
+      setTimeout(() => this.type(), typeSpeed)
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  function init() {
+      const txtElement = document.querySelector('.txt-type');
+      const words = JSON.parse(txtElement.getAttribute('data-words'));
+      const wait = txtElement.getAttribute('data-wait');
+
+      new TypeWriter(txtElement, words, wait);
+  }
